cc0d1626c7394d08b3cb59ee97d02d4d2a8a790b
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2020-11-22 13:31:28 -0500

    (WIP)

--- add_dummy.sh
+++ add_dummy.sh
@@ -3,15 +3,19 @@
 echo "Dummy"
 export MESSAGE="hoge"
 if [ "${USE_CUDA_PARALLEL}" = "00" ]; then
+    echo "00"
     export RESULT="00"
 fi
 if [ "${USE_CUDA_PARALLEL}" = "01" ]; then
+    echo "01"    
     export RESULT="01"
 fi
 if [ "${USE_CUDA_PARALLEL}" = "10" ]; then
+    echo "10"        
     export RESULT="10"
 fi
 if [ "${USE_CUDA_PARALLEL}" = "11" ]; then
+    echo "11"            
     export RESULT="11"
 fi
             
