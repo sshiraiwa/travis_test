afebc6b9e4242405e9ca1d3b2d6b4ac0a05e587f
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2020-11-22 13:18:55 -0500

    (WIP)

--- add_dummy.sh
+++ add_dummy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 echo "Dummy"
-env
+if [ "USE_CUDA_PARALLEL" = "00" ]; then
+    export RESULT="00"
+fi
+if [ "USE_CUDA_PARALLEL" = "01" ]; then
+    export RESULT="01"
+fi
+if [ "USE_CUDA_PARALLEL" = "10" ]; then
+    export RESULT="10"
+fi
+if [ "USE_CUDA_PARALLEL" = "11" ]; then
+    export RESULT="11"
+fi
+            
+
